d17b41005fc1c3518e4e0527f0ffeb91c020a172
Gabriel Rodrigues <yemolai@gmail.com>
2019-12-10 18:41:07 -0300

    [Fix] using hook to be easily reactive

--- src/stories/2-DataTable.stories.js
+++ src/stories/2-DataTable.stories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DataTable } from '../components/DataTable/DataTable'
 
 export default {
@@ -19,34 +19,50 @@ const columns = [
         format: v => `${v}`.toLocaleUpperCase()
     }
 ]
-let data = [{id: 1, name: 'John Doe'}, {id: 2, name: 'Jane Doe'}]
 
-const downloadDataHandler = () => alert('mocking download data')
-const showItemDetailsHandler = item => alert(`Item ${item.id}: ${item.name}`)
-const deleteItemsHandler = items => {
-    data = data.filter(item => {
-        const exists = items.indexOf(item) > -1
-        return !exists
-    })
+export function Table () {
+    const [data, updateData] = useState([
+        {id: 1, name: 'John Smith'},
+        {id: 2, name: 'Jane Smith'},
+        {id: 3, name: 'Jorja Smith'},
+        {id: 4, name: 'Lord Smith'}
+    ])
+    const deleteItemsHandler = items => {
+        const newData = data.filter(item => items.indexOf(item) < 0)
+        updateData(newData)
+    }
+    const showDetailsHandler = item => alert(`item #${item.id}: ${item.name}`)
+    const downloadDataHandler = item => alert(`downloadind item #${item.id}`)
+    return <DataTable
+        data={data}
+        columns={columns}
+        downloadDataHandler={downloadDataHandler}
+        handleDelete={deleteItemsHandler}
+        handleShowDetails={showDetailsHandler}
+    />
 }
 
-export const table = () => (
-    <DataTable
-        data={data}
-        columns={columns}
-        downloadDataHandler={downloadDataHandler}
-        handleDelete={deleteItemsHandler}
-        handleShowDetails={showItemDetailsHandler}
-    />
-)
-
-export const cardGrid = () => (
-    <DataTable
-        showCards
-        data={data}
-        columns={columns}
-        downloadDataHandler={downloadDataHandler}
-        handleDelete={deleteItemsHandler}
-        handleShowDetails={showItemDetailsHandler}
-    />
-)
+export function CardGrid() {
+    const [data, updateData] = useState([
+        {id: 1, name: 'John Smith'},
+        {id: 2, name: 'Jane Smith'},
+        {id: 3, name: 'Jorja Smith'},
+        {id: 4, name: 'Lord Smith'}
+    ])
+    const deleteItemsHandler = items => {
+        const newData = data.filter(item => items.indexOf(item) < 0)
+        updateData(newData)
+    }
+    const showDetailsHandler = item => alert(`item #${item.id}: ${item.name}`)
+    const downloadDataHandler = item => alert(`downloadind item #${item.id}`)
+    return (
+        <DataTable
+            showCards
+            data={data}
+            columns={columns}
+            downloadDataHandler={downloadDataHandler}
+            handleDelete={deleteItemsHandler}
+            handleShowDetails={showDetailsHandler}
+        />
+    )
+}
