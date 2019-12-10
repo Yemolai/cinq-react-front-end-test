11d221250ed5fc609dfd9a110cd491678f965c94
Gabriel Rodrigues <yemolai@gmail.com>
2019-12-10 18:40:27 -0300

    [Enhancement]  converting to functional component

--- src/components/datatable/DataTableHeader.js
+++ src/components/datatable/DataTableHeader.js
@@ -2,37 +2,34 @@ import React from 'react'
 import { throwException } from '../../utils/throwException'
 import './DataTableHeader.css'
 
-export class DataTableHeader extends React.Component {
-    state = {}
-    render () {
-        const {
-            filterChangeHandler = throwException(new Error('no filter change handler provided')),
-            deleteSelectedHandler = throwException(new Error('no delete selected handler provided')),
-            downloadDataHandler
-        } = this.props
-        const downloadAction = (
-            <div className="download wrapper column">
-                <button onClick={downloadDataHandler}>
-                    Download
+export const DataTableHeader = props => {
+    const {
+        filterChangeHandler = throwException(new Error('no filter change handler provided')),
+        deleteSelectedHandler = throwException(new Error('no delete selected handler provided')),
+        downloadDataHandler
+    } = props
+    const downloadAction = (
+        <div className="download wrapper column">
+            <button onClick={downloadDataHandler}>
+                Download
+            </button>
+        </div>
+    )
+    return (
+        <div className="datatable-header">
+            <div className="filter wrapper column">
+                <input
+                    type="text"
+                    placeholder="Filter by..."
+                    onChange={filterChangeHandler}
+                />
+            </div>
+            <div className="delete wrapper column">
+                <button onClick={deleteSelectedHandler}>
+                    Delete selected
                 </button>
             </div>
-        )
-        return (
-            <div className="datatable-header">
-                <div className="filter wrapper column">
-                    <input
-                        type="text"
-                        placeholder="Filter by..."
-                        onChange={filterChangeHandler}
-                    />
-                </div>
-                <div className="delete wrapper column">
-                    <button onClick={deleteSelectedHandler}>
-                        Delete selected
-                    </button>
-                </div>
-                {downloadDataHandler && downloadAction}
-            </div>
-        )
-    }
+            {downloadDataHandler && downloadAction}
+        </div>
+    )
 }
